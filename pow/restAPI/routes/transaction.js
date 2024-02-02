ab20f61e6971d6ebbe3d64642f32169c7bd217e7
mlibre <m.gh@linuxmail.org>
2024-02-02 20:51:17 +0330

    adding bruno and get transactionspool

--- pow/restAPI/routes/transaction.js
+++ pow/restAPI/routes/transaction.js
@@ -1,6 +1,12 @@
 const express = require( "express" );
 const router = express.Router();
 const blockchain = require( "../blockchain" );
+
+router.get( "/", function ( req, res )
+{
+	res.json( blockchain.transactionPool );
+});
+
 router.post( "/", function ( req, res, next )
 {
 	const { from, to, amount, fee, transaction_number, signature } = req.body;
